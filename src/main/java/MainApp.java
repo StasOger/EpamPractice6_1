8d65c7c3408bd67a73657dbf6d062c4b58ba3cf0
StasOger <92465858+StasOger@users.noreply.github.com>
2022-11-14 17:38:14 +0300

    70%

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -1,8 +1,52 @@
 import service.BookService;
 
+import java.net.PasswordAuthentication;
+import java.util.Properties;
+import javax.mail.*;
+import javax.mail.Message;
+import javax.mail.internet.*;
+import javax.swing.JOptionPane;
+
+
+
 public class MainApp {
+
     public static void main(String[] args) {
         BookService bookService = new BookService();
         bookService.run();
+
+//        String username = "noname.justpath@gmail.com";
+//        String password = "Dean_Winchester_jut_1";
+//
+//        Properties props = new Properties();
+//        props.put("mail.smtp.host", "smtp.gmail.com");
+//        props.put("mail.smtp.socketFactory.port", "465");
+//        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+//        props.put("mail.smtp.auth", "true");
+//        props.put("mail.smtp.port", "465");
+//
+//        Session session = Session.getDefaultInstance(props,
+//                new javax.mail.Authenticator() {
+//            protected javax.mail.PasswordAuthentication getPasswordAuthentication() {
+//                return new javax.mail.PasswordAuthentication(username, password);
+//            }
+//        });
+//        session.setDebug(true);
+//            try {
+//                Message message = new MimeMessage(session);
+//                //от кого
+//                message.setFrom(new InternetAddress("noname.justpath@gmail.com"));
+//                //кому
+//                message.setRecipients(Message.RecipientType.TO, InternetAddress.parse("stas.oger.99@yandex.by"));
+//                //тема сообщения
+//                message.setSubject("test mail");
+//                //текст
+//                message.setText("im gay");
+//                //отправляем сообщение
+//                javax.mail.Transport.send(message);
+//            } catch (MessagingException e) {
+//                e.printStackTrace();
+//            }
     }
 }
+
--- src/main/java/service/BookService.java
+++ src/main/java/service/BookService.java
@@ -50,6 +50,9 @@ public class BookService {
                 case "1":
                     showDirectory();
                     break;
+                case "2":
+                    findBook();
+                    break;
                 case "exit":
                     System.out.println("\nOver.");
                     break;
@@ -58,6 +61,52 @@ public class BookService {
 
             }
         }
+    }
+
+    private void findBook() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String choice = "";
+        String start = "Hi, Admin! \n"+
+                "- Enter \"1\" for find book by name;\n" +
+                "- Enter \"2\" for find book by author;\n" +
+                "- Enter \"3\" for find book by year;\n" +
+                "for end enter \"exit\"\n";
+        System.out.println(start);
+        while (!choice.equalsIgnoreCase("exit")) {
+            System.out.print("Enter command: ");
+            choice = reader.readLine();
+            switch (choice) {
+                case "1":
+                    findBookByName();
+                    break;
+                case "2":
+                    findBookByAuthor();
+                    break;
+                case "3":
+                    findBookByYear();
+                    break;
+                case "exit":
+                    System.out.println("\nOver.");
+                    break;
+                default:
+                    System.out.println("Incorrect. Please repeat one more time.\n");
+
+            }
+        }
+    }
+
+    private void findBookByName() {
+        System.out.println("Enter book name: ");
+        Scanner in1 = new Scanner(System.in);
+        String bookName = in1.nextLine();
+    }
+
+    private void findBookByAuthor() {
+
+    }
+
+    private void findBookByYear() {
+
     }
 
     public void adminOperation() throws IOException {
@@ -139,6 +188,25 @@ public class BookService {
         }
     }
 
+    private void addBook() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Enter name of new book: ");
+        Scanner in1 = new Scanner(System.in);
+        String bookNewName = in1.nextLine();
+
+        System.out.println("Enter author of new book: ");
+        String bookNewAuthor = in1.nextLine();
+
+        System.out.println("Enter year of new book");
+        int yearOfNewBook = in1.nextInt();
+
+        List<Book> bookList = bookRepository.selectAllBooks();
+
+        bookRepository.addBook(new Book(bookList.size()+1, bookNewName, bookNewAuthor, yearOfNewBook));
+        System.out.println("Book added!");
+    }
+
     private void firstPage() {
         List<Book> bookList = bookRepository.selectAllBooks();
         for (Book b: bookList){
@@ -165,27 +233,4 @@ public class BookService {
             }
         }
     }
-
-
-
-    private void addBook() throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
-        System.out.println("Enter name of new book: ");
-        Scanner in1 = new Scanner(System.in);
-        String bookNewName = in1.nextLine();
-
-        System.out.println("Enter author of new book: ");
-        String bookNewAuthor = in1.nextLine();
-
-        System.out.println("Enter year of new book");
-        int yearOfNewBook = in1.nextInt();
-
-        List<Book> bookList = bookRepository.selectAllBooks();
-
-        bookRepository.addBook(new Book(bookList.size()+1, bookNewName, bookNewAuthor, yearOfNewBook));
-        System.out.println("Book added!");
-    }
-
-
 }
